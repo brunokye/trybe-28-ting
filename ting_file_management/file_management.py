142dbfeb32832e71f40fa99baa7cae070748851c
Bruno Ferreira <contatobrunokye@gmail.com>
2023-06-13 19:55:58 -0300

    feat: requisito 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith('txt'):
+            return print('Formato inválido', file=sys.stderr)
+
+        with open(path_file, mode='r') as txt_file:
+            return txt_file.read().splitlines()
+
+    except FileNotFoundError:
+        return print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
