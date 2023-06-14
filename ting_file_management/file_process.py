fac400f9e9e64c5700c74a554b0a69c9f3bf91a2
Bruno Ferreira <contatobrunokye@gmail.com>
2023-06-14 14:15:44 -0300

    feat: requisito 03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        search = instance.search(index)
+        if search['nome_do_arquivo'] == path_file:
+            return
+
+    lines = txt_importer(path_file)
+
+    data = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(lines),
+        'linhas_do_arquivo': lines
+    }
+
+    instance.enqueue(data)
+    print(data, file=sys.stdout)
 
 
 def remove(instance):
