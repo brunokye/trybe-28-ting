d6449b4a23ea80346637ff377fbbc53abb525799
Bruno Ferreira <contatobrunokye@gmail.com>
2023-06-14 16:33:42 -0300

    feat: requisito 07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for index in range(len(instance)):
+        path_file = instance.search(index)
+        occurrences = []
+
+        with open(path_file['nome_do_arquivo'], 'r') as file:
+            for line_number, line in enumerate(file, start=1):
+                if word in line.lower():
+                    occurrences.append({'linha': line_number})
+
+        if occurrences:
+            result.append({
+                "palavra": word,
+                "arquivo": path_file['nome_do_arquivo'],
+                "ocorrencias": occurrences
+            })
+
+    return result
 
 
 def search_by_word(word, instance):
